6168a64bcbba614e44aab6b06667f4c5d04d7126
David PR <serallapdivad@gmail.com>
2024-01-19 14:22:46 +0000

    Updated jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,5 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	testPathIgnorePatterns: ['./dist/tests'],
-	moduleFileExtensions: ['js'],
+	moduleFileExtensions: ['ts', 'tsx', 'js'],
 }
