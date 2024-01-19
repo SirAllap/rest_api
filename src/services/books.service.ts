d7e47a5c94d12e33cc1c56c86302c1a020be8a22
David PR <serallapdivad@gmail.com>
2024-01-19 11:03:14 +0000

    Service: fixed the service to correctly return and error when the collection is empty

--- src/services/books.service.ts
+++ src/services/books.service.ts
@@ -2,8 +2,8 @@ import { BookModel } from '../models/book.model'
 
 const getAllBooks = async () => {
 	const result = await BookModel.find()
-	if (result.length > 0) {
-		throw new Error('There is no bookings in the database.')
+	if (result.length === 0) {
+		throw new Error()
 	}
 	return result
 }
