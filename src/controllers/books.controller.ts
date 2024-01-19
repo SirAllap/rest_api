51714c0fce69a8a0b6973c30e51bdd53476ee728
David PR <serallapdivad@gmail.com>
2024-01-19 11:03:28 +0000

    Controller: Improved error handler

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -1,17 +1,26 @@
 import { Router, NextFunction, Request, Response } from 'express'
 import { IBook } from '../interfaces/books.interface'
 import { bookServices } from '../services/books.service'
+import { IError } from '../interfaces/error.interface'
 
 export const BooksController = Router()
 
 BooksController.get(
 	'/',
-	async (_req: Request, res: Response<IBook[]>, next: NextFunction) => {
+	async (
+		_req: Request,
+		res: Response<IBook[] | IError>,
+		next: NextFunction
+	) => {
 		try {
 			const result = await bookServices.getAllBooks()
 			res.json(result)
 		} catch (error) {
-			next(error)
+			console.error(error)
+			return res
+				.status(404)
+				.json({ error: true, message: 'No books found', status: 404 })
 		}
+		return next()
 	}
 )
